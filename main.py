3d94c81dbb5cdaaa4707c1029375dbb48d8b5bbb
XendromeJi <136829548+XendromeJi@users.noreply.github.com>
2023-07-29 16:55:39 +0530

    Completed

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import io
 import tkinter as tk
 from tkinter import ttk, messagebox
 from PIL import Image, ImageTk
-from ttkbootstrap import style
+from ttkbootstrap import Style
 
 root = tk.Tk()
 root.title("ArtfinityPix")
